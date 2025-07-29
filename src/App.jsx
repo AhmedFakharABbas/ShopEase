a9c618bdbfcd55f243f3f1c06c91b62caff1aefd
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-07-29 15:48:19 +0500

    feat: initialize project structure and integrate Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="font-sans font-medium text-blue-300 text-2xl dark:text-sky-400 ">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
