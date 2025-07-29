59b1e6ae7444ef6f067c930dc785f0c8b32c08f2
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-07-29 15:49:09 +0500

    added commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 class="font-sans font-medium text-blue-300 text-2xl dark:text-sky-400 ">Hello world!</h1>
+      <h1 class="font-sans font-medium text-blue-300 text-2xl  dark:text-sky-400 ">Hello world!</h1>
     </>
   );
 }
